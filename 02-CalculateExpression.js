d89e1ce7dc85f77ba1664daf05a1692039818564
denisYordanov-eng <denisivov437@gmail.com>
2025-08-08 07:21:17 +0300

    Made a task.

---
+++ 02-CalculateExpression.js
@@ -0,0 +1,6 @@
+function  solve(){
+    let firstPartOfCalculate = (30 + 25) * 1/3 * (35 - 14 - 12);
+    let result = Math.pow((firstPartOfCalculate), 2);
+    //•	((30 + 25) * 1/3 * (35 - 14 - 12))2
+    console.log(result)
+}
